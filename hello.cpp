3fbef5959a8694c5ee048a1d02b1ecbda8adbe71
Hema-Sri-Ch <hemasrircpm@gmail.com>
2022-07-12 13:04:24 +0530

    Updated hello.cpp by adding some function

--- hello.cpp
+++ hello.cpp
@@ -2,11 +2,16 @@
 #include <string>
 using namespace std;
 
+void Address(string str){
+	cout << "Hello!, Welcome to learning" << str << endl;
+}
+
 int main(){
 	string name;
 	cin >> name;
 	cout << "Hello World!\n";
 	cout << "Hello " << name << endl;
+	Address(name);
 	int i;
 	for (i=0; i<5; i++){
 		cout << "loop number " << i << endl;
